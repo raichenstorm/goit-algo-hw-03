8b1c105dd0407ec744647bc3f4eab8fe5a0df8f9
raichenstorm <raichenstorm@gmail.com>
2024-03-04 20:57:48 +0200

    4

--- daysfromtoday.py
+++ daysfromtoday.py
@@ -1,15 +1,12 @@
 import datetime 
 
 def get_days_from_today(date: str) -> int:
-    try: 
-        user_input = input("Enter your date in 'YYYY-MM-DD' format: ")
-    except Exception:
-        print("Your input is not in 'YYYY-MM-DD" )
-    finally:
-        date = datetime.datetime.strptime(user_input, '%Y-%m-%d')
+    try:
+        user_date = datetime.datetime.strptime(date, '%Y-%m-%d')
         current_date = datetime.datetime.now()
-        delta_days = (current_date - date).days
+        delta_days = (current_date - user_date).days
         print(delta_days)
+    except ValueError:
+        print("Your input is not in 'YYYY-MM-DD' format ")
 
-
-get_days_from_today("2024-05-05")
+get_days_from_today("2024-03-05")
