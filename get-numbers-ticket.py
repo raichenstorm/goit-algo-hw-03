84822fc02d85b2b231933a196389e3827ef8178a
raichenstorm <raichenstorm@gmail.com>
2024-03-05 13:21:27 +0200

    7

--- get-numbers-ticket.py
+++ get-numbers-ticket.py
@@ -1,4 +1,14 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
+    random_number = set()
+    if min >= 1 and max <= 1000 and min <= quantity <= max:
+        for k in range(quantity):
+            random_number.add(random.randint(min, max))
+        print(random_number)
+    else:
+        print(random_number)
+
     
+
+get_numbers_ticket(1, 1000, 5)
