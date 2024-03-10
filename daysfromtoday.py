0dbe8e74291057bef18975c6831035b693042a00
raichenstorm <raichenstorm@gmail.com>
2024-03-10 13:03:37 +0200

    pravky1

--- daysfromtoday.py
+++ daysfromtoday.py
@@ -5,8 +5,8 @@ def get_days_from_today(date: str) -> int:
         user_date = datetime.datetime.strptime(date, '%Y-%m-%d')
         current_date = datetime.datetime.now()
         delta_days = (current_date - user_date).days
-        print(delta_days)
     except ValueError:
         print("Your input is not in 'YYYY-MM-DD' format ")
+    return delta_days
 
-get_days_from_today("2021-04-12")
+print(get_days_from_today("2024-03-11"))
--- get-numbers-ticket.py
+++ get-numbers-ticket.py
@@ -1,14 +1,28 @@
+# import random
+
+# def get_numbers_ticket(min, max, quantity):
+#     random_number = set()
+#     if min >= 1 and max <= 1000 and min <= quantity <= max:
+#         for k in range(quantity):
+#             random_number.add(random.randint(min, max))
+#         print(random_number)
+#     else:
+#         print(random_number)
+
+# get_numbers_ticket(1, 1000, 10)
+
 import random
 
 def get_numbers_ticket(min, max, quantity):
     random_number = set()
-    if min >= 1 and max <= 1000 and min <= quantity <= max:
+    if min >= 1 and max <= 1000:
         for k in range(quantity):
             random_number.add(random.randint(min, max))
-        print(random_number)
+        sorted_number = sorted(random_number)
+        sorted_set = sorted(sorted_number)
+        return sorted_set
     else:
-        print(random_number)
+        return random_number
 
-    
-
-get_numbers_ticket(1, 1000, 5)
+lottery_numbers = get_numbers_ticket(1, 1000, 12)
+print("Ваші лотерейні числа:", lottery_numbers)
