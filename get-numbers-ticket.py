cba79caa75faf608cedc487d1ac47b8181f02ffa
raichenstorm <raichenstorm@gmail.com>
2024-03-12 12:54:24 +0200

    Update get-numbers-ticket.py

--- get-numbers-ticket.py
+++ get-numbers-ticket.py
@@ -2,12 +2,15 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     random_number = set()
-    if min >= 1 and max <= 1000:
+    if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
+        print("Неправильні параметри. Перевірте, щоб min був не менше 1, max не більше 1000, а кількість чисел в діапазоні від min до max.")
+        return random_number
+    else:
         while len(random_number) < quantity:
             random_number.add(random.randint(min, max))
         return sorted(random_number)
-    else:
-        return random_number
 
-lottery_numbers = get_numbers_ticket(10, 15, -5)
-print("Ваші лотерейні числа:", lottery_numbers)
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print(f"Ваші лотерейні числа: {lottery_numbers}")
+
+# elif min >= 1 and max <= 1000:
